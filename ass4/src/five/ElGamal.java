4791037f3372b7ee3cd146c31452066470c68dfe
jerge <erikjer@student.chalmers.se>
2019-12-16 14:49:41 +0100

    fiveSolved

--- ass4/src/five/ElGamal.java
+++ ass4/src/five/ElGamal.java
@@ -3,6 +3,7 @@ package five;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.math.BigInteger;
+import java.time.Year;
 
 
 public class ElGamal {
@@ -12,7 +13,7 @@ public class ElGamal {
     }
 
     public static void main(String[] arg) {
-        String filename = "input.txt";
+        String filename = "/home/jerge/git/TDA352-ass4/ass4/src/five/input.txt";
         try {
             BufferedReader br = new BufferedReader(new FileReader(filename));
             BigInteger p = new BigInteger(br.readLine().split("=")[1]);
@@ -32,7 +33,7 @@ public class ElGamal {
             br.close();
             BigInteger m = recoverSecret(p, g, y, year, month, day, hour, minute,
                     second, c1, c2);
-            System.out.println("Recovered message: " + m);
+            System.out.println("Recovered message: \n" + m);
             System.out.println("Decoded text: " + decodeMessage(m));
         } catch (Exception err) {
             System.err.println("Error handling file.");
@@ -41,10 +42,55 @@ public class ElGamal {
         }
     }
 
+    private static BigInteger generateRandomNumber(int year, int month, int day, int hour, int minute,
+                                                   int second, BigInteger g, BigInteger p, BigInteger c1) {
+        BigInteger bigYear = BigInteger.valueOf(year);
+        BigInteger bigMonth = BigInteger.valueOf(month);
+        BigInteger bigDay = BigInteger.valueOf(day);
+        BigInteger bigHour = BigInteger.valueOf(hour);
+        BigInteger bigMinute = BigInteger.valueOf(minute);
+        BigInteger bigSecond = BigInteger.valueOf(second);
+
+        BigInteger bigAdded =
+            bigYear.multiply(BigInteger.TEN.pow(10)).add(
+            bigMonth.multiply(BigInteger.TEN.pow(8)).add(
+            bigDay.multiply(BigInteger.TEN.pow(6)).add(
+            bigHour.multiply(BigInteger.TEN.pow(4)).add(
+            bigMinute.multiply(BigInteger.TEN.pow(2)).add(
+            bigSecond
+        )))));
+
+        BigInteger r;
+        for (int i = 0; i < 999; i++) {
+            r = bigAdded.add(BigInteger.valueOf(i));
+            if (c1.equals(g.modPow(r,p))) {
+                return r;
+            }
+        }
+
+        return BigInteger.ZERO;
+    }
+
+    /**
+     *
+     * @param p the Modulus / the size of the group
+     * @param g the Generator for the group
+     * @param y the public key y=g^x (mod p), where x is the secret key
+     * @param c1 the first part of the cipher text := g^r
+     * @param c2 the second part of the cipher text := m*y^r
+     * @return
+     */
     public static BigInteger recoverSecret(BigInteger p, BigInteger g,
                                            BigInteger y, int year, int month, int day, int hour, int minute,
                                            int second, BigInteger c1, BigInteger c2) {
-        return c1;
+        BigInteger r = generateRandomNumber(year,month,day,hour,minute,second,g,p,c1);
+
+        // h^(-1) (mod p)
+        BigInteger yInv = y.modInverse(p);
+        // h^(-1)^r=h^(-r) (mod p)
+        BigInteger s = yInv.modPow(r,p);
+        // c2 = m*h^r => m*h^r*h^(-r) = m = c2*h^(-r) (mod p)
+        return c2.multiply(s).mod(p);
     }
 
 }
